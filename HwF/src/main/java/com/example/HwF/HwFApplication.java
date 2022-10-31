bf01e40260b51b372a02ae44cb11a875c526edc2
HongGunHo05 <booklike112@gmail.com>
2022-10-31 11:56:47 +0900

    Add FirstProject frame

---
+++ HwF/src/main/java/com/example/HwF/HwFApplication.java
@@ -0,0 +1,13 @@
+package com.example.HwF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwFApplication.class, args);
+	}
+
+}
