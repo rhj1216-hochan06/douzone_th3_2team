3eb2975ec762fbe1f9715f27db3593d9b4e0ba5e
UserPark0390 <seungmin0390@gmail.com>
2022-11-02 11:54:22 +0900

    qna

---
+++ HwF/src/com/hwf/controller/QnaController.java
@@ -0,0 +1,82 @@
+package com.hwf.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hwf.dao.QnaDAO;
+import com.hwf.model.QnaDTO;
+
+
+@WebServlet("/views/board")
+//@WebServlet("/board")
+public class QnaController extends HttpServlet {
+	
+	@Autowired
+	public QnaController() { }
+	
+	public void service(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+	
+		request.setCharacterEncoding("UTF-8"); // 한글로 데이터를 받고 내려가야함 
+		
+		String cmd = request.getParameter("cmd");  //  ~~?cmd=list
+		System.out.println("cmd : " + cmd);
+
+		if( cmd.equals("list") ) {			
+			list(request, response);
+		} else if( cmd.equals("write") ) {			
+			write(request, response);
+		} 
+	} // service end
+
+	public void list(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+
+		QnaDAO dao = new QnaDAO();
+		List<QnaDTO>  list = dao.selectAll();
+		
+		if( list != null ) {
+			request.setAttribute("list", list);  // data save
+			request.getRequestDispatcher("/views/jsp/list.jsp").forward(request, response);
+//			request.getRequestDispatcher("/views/list.jsp").forward(request, response);
+		} else {
+			response.sendRedirect("/views/jsp/error.jsp");
+		}
+	} // list end
+	
+	
+	
+	
+	  public void write(HttpServletRequest request, HttpServletResponse response)
+	  throws ServletException, IOException {
+	 
+			request.setCharacterEncoding("UTF-8");
+			  String memberid = request.getParameter("memberid"); 
+			  String qnacontent = request.getParameter("qnacontent"); 
+			  String qnaanswer = request.getParameter("qnaanswer");
+			
+			  System.out.println(qnacontent + ",\t" + qnaanswer +",\t" + memberid);
+			
+			QnaDAO dao = new QnaDAO();
+			QnaDTO dto = new QnaDTO(qnacontent, qnaanswer, memberid);
+			System.out.println(dto);
+			int rowcount = dao.insert(dto);
+			System.out.println(dao);
+			
+			if( rowcount > 0 ) {
+				response.sendRedirect("board?cmd=list");
+//				response.sendRedirect("/HwF/views/jsp/list.jsp");
+//				response.sendRedirect("/HwF/views/list.jsp");
+			} else {
+				response.sendRedirect("/views/jsp/error.jsp");
+			}
+		} // write end
+}
