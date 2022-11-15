72ff4bec985c761f153a99a9caf432934c9aa1e2
HongGunHo05 <booklike112@gmail.com>
2022-11-15 17:25:50 +0900

    프로젝트 완료

---
+++ HwF/webapp/views/js/OrderSelectJS.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+	/* 수령기간 (영양제 아니면 '-' 형식으로 뜨도록) */
+	var arr2 = document.getElementsByClassName("receiptPeriod");
+
+	for (var i = 0; i < arr2.length; i++) {
+		if (arr2[i].innerText == 0) {
+			arr2[i].innerText = '-';
+		} else {
+			arr2[i].innerText = arr2[i].innerText + '일';
+		}
+	}
+
+	/* 수령방식 (영양제 아니면 '-' 형식으로 뜨도록) */
+	var arr3 = document.getElementsByClassName("purchaseReceipt");
+
+	for (var i = 0; i < arr3.length; i++) {
+		if (arr3[i].innerText == 0) {
+			arr3[i].innerText = '-';
+		}
+	}
+
+	/* 수령시작일 (영양제 아니면 '-' 형식으로 뜨도록) */
+	var arr4 = document.getElementsByClassName("receiptStartDate");
+
+	for (var i = 0; i < arr4.length; i++) {
+		if (arr4[i].innerText == 0) {
+			arr4[i].innerText = '-';
+		}
+	}
+
+
+}
